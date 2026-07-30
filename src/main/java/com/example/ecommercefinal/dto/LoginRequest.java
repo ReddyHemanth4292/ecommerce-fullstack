013f2fbf6a33d6768743004df95f73cdde1ea341
ReddyHemanth4292 <rq0665@srmist.edu.in>
2026-07-30 10:25:40 +0530

    Week 6 Day 1 - Complete Swagger OpenAPI documentation

--- src/main/java/com/example/ecommercefinal/dto/LoginRequest.java
+++ src/main/java/com/example/ecommercefinal/dto/LoginRequest.java
@@ -1,15 +1,22 @@
 package com.example.ecommercefinal.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(description = "User login request")
 public class LoginRequest {
+    @Schema(description = "Registered email address",
+            example = "hemanth@example.com")
     @Email(message = "Invalid email format")
     @NotBlank(message = "Email is required")
     private String email;
+
+    @Schema(description = "Registered email address",
+            example = "hemanth@example.com")
     @NotBlank(message = "Password is required")
     private String password;
 }
